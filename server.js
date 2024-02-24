640811de8cfa42f01f22668f2a980cdb5abb6bf0
Niko <niko.karamitos@gmail.com>
2024-02-24 00:33:22 -0500

    imma try somethin

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
-
-
 // Use an environment variable for the MongoDB URI
 const uri = process.env.MONGODB_URI;
 
