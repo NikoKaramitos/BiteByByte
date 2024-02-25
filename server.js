efe15cfac9a2348c28b6df0eb38bdebfc6f3135a
giovannimv <giovannimaldonado07@gmail.com>
2024-02-24 21:06:23 -0500

    Added api register & login

--- server.js
+++ server.js
@@ -48,3 +48,68 @@ app.listen(PORT, () => {
   console.error('Express failed to start');
   console.error(err);
 });
+
+app.post('/api/register', async (req, res, next) =>
+{
+  // incoming: firstName, lastName, email, username, password
+  // outgoing: error
+	
+  const { userId, firstName, lastName, email, login, password } = req.body;
+
+  const newUsers = {
+    UserId:userId,
+    FirstName:firstName,
+    LastName:lastName,
+    Email:email,
+    Login:login,
+    Password:password};
+  
+  var error = '';
+
+  try
+  {
+    const db = client.db("Users");
+    try
+    {
+        const result = await db.collection('users').insertOne(newUsers);
+    }
+    catch(e)
+    {
+        error = e.toString();
+    }
+  }
+  catch(e)
+  {
+    error = e.toString();
+  }
+
+  var ret = { error: error };
+  res.status(200).json(ret);
+});
+
+app.post('/api/login', async (req, res, next) => 
+{
+  // incoming: login, password
+  // outgoing: id, firstName, lastName, error
+ 
+  var error = '';
+
+  const { login, password } = req.body;
+
+  const db = client.db("Users");
+  const results = await db.collection('users').find({Login:login,Password:password}).toArray();
+
+  var id = -1;
+  var fn = '';
+  var ln = '';
+
+  if( results.length > 0 )
+  {
+    id = results[0]._id;
+    fn = results[0].FirstName;
+    ln = results[0].LastName;
+  }
+
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
+  res.status(200).json(ret);
+});
