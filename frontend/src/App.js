0137ee8f96d19cc7b34f3b824a39a732cf8258ce
rachelv026 <rachelvitaliano@gmail.com>
2024-03-05 14:09:05 -0500

    install tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/navbar";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ import CardPage from "./pages/CardPage";
 function App() {
 	return (
 		<BrowserRouter>
+			<Navbar/>
 			<Routes>
 				<Route path="/" index element={<LoginPage />} />
 				<Route path="/cards" index element={<CardPage />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
