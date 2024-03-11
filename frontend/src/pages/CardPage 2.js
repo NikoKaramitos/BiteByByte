4ac421a5802789120287178593b65c0dd37f5333
NikoKaramitos <niko.karamitos@gmail.com>
2024-03-11 19:58:42 -0400

    updated json package again

---
+++ frontend/src/pages/CardPage 2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import LoggedInName from '../components/LoggedInName';
+import CardUI from '../components/CardUI';
+
+const CardPage = () =>
+{
+    return(
+        <div>
+
+            <LoggedInName />
+            <CardUI />
+        </div>
+    );
+}
+
+export default CardPage;
---
+++ frontend/src/pages/RegisterPage 2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import Login from '../components/Login';
+import Navbar from '../components/navbar';
+import Register from '../components/Register';
+
+const RegisterPage = () =>
+{
+
+    return(
+      <div>
+        <><Navbar/></>
+        <Register />
+        
+      </div>
+    );
+}
+
+export default RegisterPage;
+
