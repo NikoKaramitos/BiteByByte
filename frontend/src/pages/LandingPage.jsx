647a19e374b69e550179779a5b2fc3ede345bfc5
ximena99v <xramirezrivera@knights.ucf.edu>
2024-03-11 13:30:58 -0400

    Revert "link login/register buttons and change login/register colors"
    This reverts commit 14372a404e33d30fd8627b7161ef3f454a70830a.

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -4,22 +4,16 @@ import {motion} from "framer-motion";
 import { Button } from "@material-tailwind/react";
 import Navbar from '../components/navbar';
 import Body from "../pages/Body";
-import Footer from "../pages/Footer";
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Footer from "../components/Footer"
 
 
 const LandingPage = () =>{
-	const navigate = useNavigate();
-
 	return (
 		<div>
-			<Navbar/>
 	  		<h1 className='text-black text-2xl text-center mt-20'>Learn the cuisine’s of the world!</h1>
 			  <div className="flex flex-col items-center justify-center">
-				<Button className='btn h-12 w-1/3 text-color-black border-solid border-2 border-black rounded-lg mt-14 bg-emerald-400 hover:bg-emerald-500'
-				onClick={() => navigate('/register')}>Get Started</Button>
-				<Button className='btn h-12 w-1/3 text-color-black border-solid border-2 border-black rounded-lg mt-8 bg-slate-100 hover:bg-slate-200'
-				onClick={() => navigate('/login')}>I already have an account</Button>
+				<Button className='btn h-12 w-1/3 text-color-black border-solid border-2 border-black rounded-lg mt-14 bg-emerald-400 hover:bg-emerald-500'>Get Started</Button>
+				<Button className='btn h-12 w-1/3 text-color-black border-solid border-2 border-black rounded-lg mt-8 bg-slate-100 hover:bg-slate-200'>I already have an account</Button>
         	</div>
 			<motion.div animate={{rotate: 360}} transition={{delay: 1}}>
 			<img className="mt-16 mb-20 h-60 w-60 rounded-full mx-auto items-center justify-center"
@@ -28,9 +22,6 @@ const LandingPage = () =>{
 			alt="Earth"
 			/>
 			</motion.div>
-
-			<Body/>
-			<Footer/>
 		</div>
 		
 	)
