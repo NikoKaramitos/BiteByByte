67543436d75c8769f3a3a7c423bc62e1c8a9a96e
ximena99v <xramirezrivera@knights.ucf.edu>
2024-03-28 00:43:15 -0400

    apearance is done
    apearance is down, still need to work on functionality

--- frontend/src/components/CuisinePicker.jsx
+++ frontend/src/components/CuisinePicker.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import bearPR from "../assets/bearPr.png";
 import frenchBear from "../assets/cuisineBears/frenchBear.png";
 import chinaBear from "../assets/cuisineBears/chinaBear.png";
 import mexicoBear from "../assets/cuisineBears/mexicoBear.png";
+import italyBear from "../assets/cuisineBears/italyBear.png";
 import window from "../assets/Designer.jpeg";
 
 export default function CuisinePicker() {
@@ -92,42 +92,82 @@ export default function CuisinePicker() {
         src={window}
         alt=""
       />
-      <div
-        className="flex relative
-		justify-center items-center container shadow-2xl mx-auto h-64
-		max-w-[800px] w-full rounded 2xl shadowl border-4 border-emerald-900 2xl bg-emerald-500"
-      >
-        <h2
-          className="animate-bounce text-xl leading-none tracking-tight text-transform: capitalize font-bold text-center py-8"
-          style={{ lineHeight: "3em" }}
+
+<div>
+      <div className="flex p-12 justify-center items-center h-full">
+        <div
+          className="flex relative
+          justify-center items-center container shadow-2xl mx-auto 
+          max-w-[800px] w-full rounded 2xl shadowl border-4 border-[#fc8f22] 2xl bg-orange-50 text-[#009ea2]"
         >
-          Hello Chef {firstName}!<br></br>
-          Where would you like to begin?
-        </h2>
+          <div>
+          <h2
+            className="animate-bounce text-xl leading-none tracking-tight text-transform: capitalize font-bold text-center py-6 p-4"
+            style={{ lineHeight: "3em" }}
+          >
+            Hello Chef {firstName}!<br></br>
+            Where would you like to begin?
+          
+          </h2>
+          <p class=" flex justify-center p-4 text-xs text-center text-gray-500 dark:text-gray-600">
+                        Select the cuisine you wish to master
+                    </p>
+          </div>
+        </div>
       </div>
 
-      <div class="justify-center items-center grid grid-cols-2 m-4 p-5">
-        <div className="relative flex m-4 justify-center items-center">
-          <button
-            buttonType="filled"
-            size="xl"
-            rounded={false}
-            block={false}
-            iconOnly={false}
-            ripple={true}
-            onClick={addCard}
-            className=" hover:bg-emerald-400 border-b-4 border-emerald-700 hover:border-emerald-500
-            align-middle font-bold text-center uppercase transition-all text-lg
-				disabled:opacity-50  py-3 px-6 rounded-lg bg-emerald-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg
-				 hover:shadow-gray-900/20 focus:opacity-[0.85]
-				 active:opacity-[0.85] active:shadow-none flex items-center w-80 h-40"
-          >
-            <img
-              src={bearPR}
-              alt="BearPR"
-              className="h-20 w-20 mr-5 font-bold"
-            />
-            <h1 className="text-center text-3xl">italy</h1>
+      <div class="justify-center items-center grid grid-cols-2 m-8 p-5">
+        <div className=" relative flex m-8 justify-center">
+          <button className="w-full sm:w-auto">
+            <a
+              href="#_"
+              class="relative  inline-flex items-center justify-start py-7 pl-8 pr-16 overflow-hidden font-semibold
+             text-[#009246] transition-all duration-150 ease-in-out rounded-lg hover:pl-10 hover:pr-6 bg-gray-50 group"
+            >
+              <span
+                class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out
+             bg-[#009246] group-hover:h-full"
+              ></span>
+              <span class="absolute right-2 pr-4 duration-200 ease-out group-hover:translate-x-16">
+                <svg
+                  class="w-5 h-5 text-[#ce2b37]"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                <svg
+                  class="w-5 h-5 text-[#ce2b37]"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span
+                class="relative w-full text-left transition-colors duration-200 ease-in-out
+             group-hover:text-white p-2"
+              >
+                ITALY
+              </span>
+              <img src={italyBear} alt="italyBear" className=" h-21 w-20 " />
+            </a>
           </button>
         </div>
 
@@ -293,6 +333,7 @@ export default function CuisinePicker() {
             </a>
           </button>
         </div>
+      </div>
       </div>
     </div>
   );
