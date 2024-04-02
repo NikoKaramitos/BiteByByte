9c0150214bb5f5da9fd7102800ed686cce4e2090
rachelv026 <rachelvitaliano@gmail.com>
2024-04-02 19:44:58 -0400

    update navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -13,44 +13,40 @@ const Navbar = () => {
         navigate('/');
     }
 
-    
     return (
         <div className='nav navbar bg-orange-50 border-gray-200 sticky top-0'>
             <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-            <a href="https://github.com/NikoKaramitos/BiteByByte" className="flex items-center space-x-3 rtl:space-x-reverse">
-    <img src={bear} className="h-14" alt="BiteByByteLogo" />
-    <span className="self-center">
-      <span className={`text-xl font-semibold whitespace-nowrap ${isDashPage() ? 'text-red-500' : 'text-black'}`} style={{ textShadow: '1px 1px 1px black' }}>Bite</span>
-      <span className={`text-xl font-semibold whitespace-nowrap ${isDashPage() ? 'text-white' : 'text-black'}`} style={{ textShadow: '1px 1px 1px black' }}>By</span>
-      <span className={`text-xl font-semibold whitespace-nowrap ${isDashPage() ? 'text-green-500' : 'text-black'}`} style={{ textShadow: '1px 1px 1px black' }}>Byte</span>
-    </span>
-</a>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
-            <li className='hover:text-emerald-400'>
-                <NavLink to="/">Home</NavLink>
-                </li>
-                <li className='hover:text-emerald-400'>
-                <NavLink to="about">About</NavLink>
-                </li>
-                <li className='hover:text-emerald-400'>
-                <NavLink to="settings">Settings</NavLink>
-                </li>
-                    {
-                        auth ? ( <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
-                            <li>
-                            <NavLink onClick={logOut} to="/">Logout</NavLink>
-                            </li>
-                            </ul>
-                        ) :<>
-                        </>
-                    }
-                </ul>
+                <a href="https://github.com/NikoKaramitos/BiteByByte" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src={bear} className="h-14" alt="BiteByByteLogo" />
+                    <span className="self-center">
+                        <span className={`text-xl font-semibold whitespace-nowrap ${isDashPage() ? 'text-red-500' : 'text-black'}`} style={{ textShadow: '1px 1px 1px black' }}>Bite</span>
+                        <span className={`text-xl font-semibold whitespace-nowrap ${isDashPage() ? 'text-white' : 'text-black'}`} style={{ textShadow: '1px 1px 1px black' }}>By</span>
+                        <span className={`text-xl font-semibold whitespace-nowrap ${isDashPage() ? 'text-green-500' : 'text-black'}`} style={{ textShadow: '1px 1px 1px black' }}>Byte</span>
+                    </span>
+                </a>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
+                        <li className='hover:text-emerald-400'>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li className='hover:text-emerald-400'>
+                            <NavLink to="about">About</NavLink>
+                        </li>
+                        {auth ? (
+                            <>
+                                <li className='hover:text-emerald-400'>
+                                    <NavLink to="settings">Settings</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink onClick={logOut} to="/">Logout</NavLink>
+                                </li>
+                            </>
+                        ) : null}
+                    </ul>
                 </div>
             </div>
         </div>
     )
 }
-
 
 export default Navbar;
